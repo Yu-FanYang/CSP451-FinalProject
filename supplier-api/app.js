04b42ff8d8d5ba60b82a86b36918c2fe0a5ced39
Yu-FanYang <charcoalchocolate@gmail.com>
2025-08-03 19:56:56 +0800

    test Azure Key Vault, add supplier-api

--- supplier-api/app.js
+++ supplier-api/app.js
@@ -3,9 +3,23 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
+// add API Key
+const EXPECTED_API_KEY = process.env.API_KEY || 'no-key-configured';
+
+const apiKeyAuth = (req, res, next) => {
+  const userApiKey = req.get('x-api-key'); // from request header read 'x-api-key'
+
+  if (userApiKey && userApiKey === EXPECTED_API_KEY) {
+    next(); // Key ok
+  } else {
+    // key error, return 401
+    res.status(401).json({ error: 'Unauthorized. Invalid or missing API Key.' });
+  }
+};
+
 app.use(express.json()); 
 
-app.post('/order', (req, res) => { 
+app.post('/order', apiKeyAuth, (req, res) => { 
   const correlationId = req.body.correlationId || 'N/A'; 
   const product = req.body.product || 'Unknown Product';
   const quantity = req.body.quantity || 0;
@@ -17,11 +31,7 @@ app.post('/order', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker on Azure VM! This is version 2.0.<br>' +
-            'Hello from Docker on Azure VM! This is version 2.0. add test CI<br>' +
-            'Hello from Docker on Azure VM! This is version 2.0. add test CI 2<br>' +
-            'Hello from Docker on Azure VM! This is version 2.0. add test CI 3<br>'
-          );
+  res.send('Hello from Docker on Azure VM! This is version 3.0 with API Key Auth.');
 });
 
 app.listen(port, () => {
